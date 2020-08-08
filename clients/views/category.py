a435e52467993fe96471ea5b9402467f7357ad68
farzana <farzana09bd@gmail.com>
2020-08-08 21:15:55 +0600

    Remove old image when delete or update

--- clients/views/category.py
+++ clients/views/category.py
@@ -5,20 +5,11 @@ from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
 from clients.forms.product import CategoryForm
 from products.models import Category
-from django.db.models import Q
 
 
 class CategoryListView(ListView):
     model = Category
     template_name = 'clients/category/list.html'
-
-    def get_context_data(self, **kwargs):
-        # Call the base implementation first to get a context
-        context = super().get_context_data(**kwargs)
-        # Add in a QuerySet of all the books
-        context['object_list'] = Category.objects.filter(
-            parent_id=None)
-        return context
 
 
 class CategoryCreateView(SuccessMessageMixin, CreateView):
