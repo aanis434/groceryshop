7d4613fcef97d562d400b706107dc845ffc0cea3
aanis434 <aanis434@gmail.com>
2020-08-10 02:18:44 +0600

    product crud done

--- clients/views/product.py
+++ clients/views/product.py
@@ -4,7 +4,7 @@ from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
 from clients.forms.product import ProductForm
-from products.models import Product
+from products.models import Product, Category
 
 
 class ProductListView(ListView):
@@ -17,6 +17,15 @@ class ProductCreateView(SuccessMessageMixin, CreateView):
     model = Product
     template_name = 'clients/product/create.html'
     success_message = "%(name)s saved."
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        categories()
+        self.categories()
+        return context
+
+    def categories(self):
+        print('self fun')
 
 
 class ProductUpdateView(SuccessMessageMixin, UpdateView):
@@ -35,3 +44,9 @@ class ProductDeleteView(SuccessMessageMixin, DeleteView):
         obj = self.get_object()
         messages.success(self.request, self.success_message % obj.__dict__)
         return super().delete(request, *args, **kwargs)
+
+
+def categories():
+    pass
+
+
