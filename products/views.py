f58ba8a3abfb68a48037e83d58ee514ca046b4fd
farzana <farzana09bd@gmail.com>
2020-08-23 13:42:14 +0600

    sidebar collapsing

--- products/views.py
+++ products/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+from helpers import generate_category
+from products.models import Category
+
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['header_categories'] = generate_category()
+        context['categories'] = Category.objects.all()
+        return context
