6a0b028fbedad2409b4fcbab9546f88fbb00bd0b
aanis434 <aanis434@gmail.com>
2020-08-28 22:05:46 +0600

    update frontend template

--- assets/frontend/js/custom_script.js
+++
@@ -1,6 +0,0 @@
-// Prevent closing from click inside dropdown
-$(document).on('click', '.submenu', function (e) {
-    var current=$(this).next();
-    current.toggle();
-    e.stopPropagation();
-});
